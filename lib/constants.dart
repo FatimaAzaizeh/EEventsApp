6fb9c0cca3493695353235a66c56a72ced6a91d5
Raneemkalbouneh <162698624+Raneemkalbouneh@users.noreply.github.com>
2024-04-03 14:19:01 +0300

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-
 //The colors to be used in the app and the required padding space.
 
-
-
-/*
-const kBackgroundColor = Color(0xFFF1EFF1);
-const kPrimaryColor = Color(0xFF219ebc);
-const kSecondaryColor = Color(0xFFfcca46);
-const kTextColor = Color(0xFF023047);
-const kTextLightColor = Color(0xFF747474);
-const kBlueColor = Color(0xFF40BAD5);
+const kcolor0 = Color.fromARGB(255, 30, 14, 66);
+const kColor1 = Color.fromRGBO(57, 36, 105, 1);
+const kColor2 = Color.fromRGBO(80, 53, 135, 1);
+const kColor3 = Color.fromARGB(255, 143, 113, 184);
+const kColor4 = Color.fromARGB(255, 187, 163, 228);
+const kColor5 = Color.fromARGB(255, 146, 146, 240);
+const kColor6 = Color.fromARGB(255, 161, 162, 223);
+const kColor7 = Color.fromRGBO(247, 239, 229, 1);
+const kColor8 = Color.fromRGBO(255, 251, 245, 1);
 
 const kDefaultPadding = 20.0;
-*/
