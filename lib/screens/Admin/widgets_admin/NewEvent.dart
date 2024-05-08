18f8caa0356c7be415a5bfb5d8e2fe59c9bbf35f
Raneemkalbouneh <162698624+Raneemkalbouneh@users.noreply.github.com>
2024-05-08 12:23:24 +0300

    Update NewEvent.dart

--- lib/screens/Admin/widgets_admin/NewEvent.dart
+++ lib/screens/Admin/widgets_admin/NewEvent.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:quickalert/models/quickalert_type.dart';
 import 'package:quickalert/widgets/quickalert_dialog.dart';
@@ -8,8 +7,6 @@ import 'package:testtapp/constants.dart';
 import 'package:testtapp/models/EventType.dart';
 import 'package:testtapp/screens/Admin/widgets_admin/TexFieldDesign.dart';
 import 'package:testtapp/widgets/Event_item.dart';
-
-// Other import statements and code
 
 final _firestore = FirebaseFirestore.instance;
 
@@ -33,21 +30,20 @@ class _AddEventState extends State<AddEvent> {
   late String ImageUrl;
   bool showSpinner = false;
   late String dropdownValue;
-  List<String> classificationList = []; // Initialize classification list
+  List<String> classificationList = [];
   late String id;
   bool isButtonEnabled = false;
 
-  // Method to fetch classifications from Firestore
   void fetchClassificationsFromFirestore() async {
     try {
       setState(() {
-        showSpinner = true; // Show spinner when fetching data
+        showSpinner = true;
       });
       QuerySnapshot querySnapshot =
           await _firestore.collection("Classifications").get();
 
       setState(() {
-        classificationList.clear(); // Clear the list before adding new data
+        classificationList.clear();
       });
 
       for (var docSnapshot in querySnapshot.docs) {
@@ -55,36 +51,32 @@ class _AddEventState extends State<AddEvent> {
       }
       setState(() {
         dropdownValue = classificationList.first;
-        showSpinner = false; // Hide spinner after fetching data
+        showSpinner = false;
       });
     } catch (e) {
       print("Error fetching classifications: $e");
       setState(() {
-        showSpinner = false; // Hide spinner if there's an error
+        showSpinner = false;
       });
     }
   }
 
-//edit
   Future<void> getDataById(String documentId) async {
     DocumentSnapshot snapshot = await FirebaseFirestore.instance
         .collection('EventType')
-        .doc(documentId) // Use document ID directly
+        .doc(documentId)
         .get();
 
     if (snapshot.exists) {
       setState(() {
-        // Document with the provided ID exists
         ControllerName.text = snapshot.get('Name');
         ControllerSer.text = snapshot.get('ServiceType').toString();
         ControllerImage.text = snapshot.get('imageUrl');
-        //  dropdownValue = snapshot.get('Classification');
         showEditButton = true;
         id = documentId;
         isButtonEnabled = true;
       });
     } else {
-      // Document with the provided ID does not exist
       print("Document not found for ID: $documentId");
     }
   }
@@ -131,9 +123,7 @@ class _AddEventState extends State<AddEvent> {
             Navigator.of(context).pop();
           },
           onCancelBtnTap: () {
-            // Handle cancellation action here
-            // For example, you can dismiss the dialog or perform any other action.
-            Navigator.of(context).pop(); // Dismiss the dialog
+            Navigator.of(context).pop();
           });
     });
   }
@@ -141,149 +131,121 @@ class _AddEventState extends State<AddEvent> {
   @override
   void initState() {
     super.initState();
-    fetchClassificationsFromFirestore(); // Fetch classifications when widget is initialized
+    fetchClassificationsFromFirestore();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment
-          .spaceAround, // Distributes space evenly between the children
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Color.fromARGB(221, 255, 255, 255)),
-            width: 500,
-            height: double.maxFinite,
-            child: Column(
-              children: [
-                Container(
-                  width: double.maxFinite,
-                  height: 70,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30))),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Text('المناسبات',
-                            textAlign: TextAlign.center,
+    return SingleChildScrollView(
+      // Wrap with SingleChildScrollView
+      scrollDirection: Axis.horizontal, // Allow horizontal scrolling
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color.fromARGB(221, 255, 255, 255)),
+              width: 500,
+              height: double.maxFinite,
+              child: SingleChildScrollView(
+                // Wrap the Column with SingleChildScrollView
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: double.maxFinite,
+                      height: 70,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30))),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text('المناسبات',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontFamily: 'Amiri',
+                                    fontSize: 28,
+                                    fontStyle: FontStyle.italic,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 60, 19, 60))),
+                            SizedBox(
+                              width: 220,
+                            ),
+                            // Edit, Delete, Clear buttons...
+                          ],
+                        ),
+                      ),
+                    ),
+                    TextFieldDesign(
+                      Text: 'أسم المناسبة:',
+                      icon: Icons.title,
+                      ControllerTextField: ControllerName,
+                      onChanged: (value) {
+                        name = value;
+                      },
+                      obscureTextField: false,
+                    ),
+                    DropdownButton<String>(
+                      value:
+                          classificationList.isNotEmpty ? dropdownValue : null,
+                      icon: const Icon(Icons.arrow_downward),
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.deepPurple),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String? value) {
+                        setState(() {
+                          classification = value!;
+                          dropdownValue = value;
+                        });
+                      },
+                      items: classificationList
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                    TextFieldDesign(
+                      Text: 'الخدمات المتعلقة بالمناسبة:',
+                      icon: Icons.room_service,
+                      ControllerTextField: ControllerSer,
+                      onChanged: (value) {
+                        serviceType = value.split(',');
+                      },
+                      obscureTextField: false,
+                    ),
+                    TextFieldDesign(
+                      Text: 'إضافة صورة:',
+                      icon: Icons.image,
+                      ControllerTextField: ControllerImage,
+                      onChanged: (value) {
+                        ImageUrl = value;
+                      },
+                      obscureTextField: false,
+                    ),
+                    Container(
+                      width: double.maxFinite,
+                      margin: EdgeInsets.only(bottom: 90),
+                      child: FloatingActionButton(
+                        backgroundColor: ColorPink_100,
+                        child: Text('إضافة مناسبة',
                             style: TextStyle(
                                 fontFamily: 'Amiri',
-                                fontSize: 28,
+                                fontSize: 18,
                                 fontStyle: FontStyle.italic,
-                                fontWeight: FontWeight.bold,
-                                color: Color.fromARGB(255, 60, 19, 60))),
-                        SizedBox(
-                          width: 220,
-                        ),
-                        TextButton(
-                          onPressed: isButtonEnabled
-                              ? () {
-                                  EditEvent(id);
-                                  isButtonEnabled = false;
-                                }
-                              : null,
-                          child: Icon(Icons.edit),
-                        ),
-                        TextButton(
-                            onPressed: isButtonEnabled
-                                ? () {
-                                    DelEvent(id);
-                                    isButtonEnabled = false;
-                                  }
-                                : null,
-                            child: Icon(Icons.delete)),
-                        TextButton(
-                            onPressed: () {
-                              setState(() {
-                                ControllerName.clear();
-                                ControllerSer.clear();
-                                ControllerImage.clear();
-                                isButtonEnabled = false;
-                              });
-                            },
-                            child: Icon(Icons.clear)),
-                      ],
-                    ),
-                  ),
-                ),
-
-                TextFieldDesign(
-                    Text: 'أسم المناسبة:',
-                    icon: Icons.title,
-                    ControllerTextField: ControllerName,
-                    onChanged: (value) {
-                      name = value; // Update the Name variable
-                      // Update the serviceType list
-                    },
-                    obscureTextField: false),
-
-                // Call the buildClassificationDropdown method where you want to display the dropdown
-
-                DropdownButton<String>(
-                  value: classificationList.isNotEmpty ? dropdownValue : null,
-                  icon: const Icon(Icons.arrow_downward),
-                  elevation: 16,
-                  style: const TextStyle(color: Colors.deepPurple),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.deepPurpleAccent,
-                  ),
-                  onChanged: (String? value) {
-                    // This is called when the user selects an item.
-                    setState(() {
-                      classification = value!;
-                      dropdownValue = value;
-                    });
-                  },
-                  items: classificationList
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-
-                TextFieldDesign(
-                    Text: 'الخدمات المتعلقة بالمناسبة:',
-                    icon: Icons.room_service,
-                    ControllerTextField: ControllerSer,
-                    onChanged: (value) {
-                      serviceType =
-                          value.split(','); // Update the serviceType list
-                    },
-                    obscureTextField: false),
-                TextFieldDesign(
-                    Text: 'إضافة صورة:',
-                    icon: Icons.image,
-                    ControllerTextField: ControllerImage,
-                    onChanged: (value) {
-                      ImageUrl = value;
-                    },
-                    obscureTextField: false),
-
-                Container(
-                    width: double.maxFinite,
-                    margin: EdgeInsets.only(bottom: 90),
-                    child: FloatingActionButton(
-                      backgroundColor: ColorPink_100,
-                      child: Text('إضافة مناسبة',
-                          style: TextStyle(
-                              fontFamily: 'Amiri',
-                              fontSize: 18,
-                              fontStyle: FontStyle.italic,
-                              color: Colors.white)),
-                      onPressed: () {
-                        setState(() async {
-                          // Perform Firestore query based on the selected dropdown value
+                                color: Colors.white)),
+                        onPressed: () async {
                           QuerySnapshot<Map<String, dynamic>> querySnapshot =
                               await FirebaseFirestore.instance
                                   .collection('Classifications')
@@ -304,26 +266,28 @@ class _AddEventState extends State<AddEvent> {
                             context: context,
                             type: QuickAlertType.success,
                           );
-                        });
-                      },
-                    )),
-              ],
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              width: 400,
+              height: double.maxFinite,
+              child: EventScreen(),
             ),
-            width: 400,
-            height: double.maxFinite,
-            child: EventScreen(),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
