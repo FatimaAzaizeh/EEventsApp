c12515e988d3f825cf63a228e710a9a47b1be4aa
FatimaAzaizeh <202110683@ammanu.edu.jo>
2024-05-08 16:33:19 +0300

    update

--- lib/screens/Admin/widgets_admin/NewEvent.dart
+++ lib/screens/Admin/widgets_admin/NewEvent.dart
@@ -26,7 +26,6 @@ class _AddEventState extends State<AddEvent> {
   final _auth = FirebaseAuth.instance;
   late String name; // Name of the Event
   late String classification = ''; // Classification of the Event
-  late List<String> serviceType = []; // Services related to the event
   late String ImageUrl;
   bool showSpinner = false;
   late String dropdownValue;
@@ -39,18 +38,22 @@ class _AddEventState extends State<AddEvent> {
       setState(() {
         showSpinner = true;
       });
+
       QuerySnapshot querySnapshot =
-          await _firestore.collection("Classifications").get();
+          await _firestore.collection("event_classificaion_types").get();
 
       setState(() {
         classificationList.clear();
       });
 
       for (var docSnapshot in querySnapshot.docs) {
-        classificationList.add(docSnapshot.get('Name').toString());
+        classificationList.add(docSnapshot.get('description').toString());
       }
+
       setState(() {
-        dropdownValue = classificationList.first;
+        dropdownValue = (classificationList.isNotEmpty
+            ? classificationList.first.toString()
+            : null)!;
         showSpinner = false;
       });
     } catch (e) {
@@ -63,15 +66,15 @@ class _AddEventState extends State<AddEvent> {
 
   Future<void> getDataById(String documentId) async {
     DocumentSnapshot snapshot = await FirebaseFirestore.instance
-        .collection('EventType')
+        .collection('event_types')
         .doc(documentId)
         .get();
 
     if (snapshot.exists) {
       setState(() {
-        ControllerName.text = snapshot.get('Name');
-        ControllerSer.text = snapshot.get('ServiceType').toString();
-        ControllerImage.text = snapshot.get('imageUrl');
+        ControllerName.text = snapshot.get('name');
+
+        ControllerImage.text = snapshot.get('image_url');
         showEditButton = true;
         id = documentId;
         isButtonEnabled = true;
@@ -84,12 +87,12 @@ class _AddEventState extends State<AddEvent> {
   void EditEvent(String documentId) {
     setState(() {
       var docRef =
-          FirebaseFirestore.instance.collection('EventType').doc(documentId);
+          FirebaseFirestore.instance.collection('event_types').doc(documentId);
 
       docRef.update({
-        'Name': ControllerName.text,
-        'ServiceType': ControllerSer.text,
-        'imageUrl': ControllerImage.text,
+        'name': ControllerName.text,
+        'event_classificaion_types': ControllerSer,
+        'image_url': ControllerImage.text,
       }).then((_) {
         print("Document updated successfully.");
       }).catchError((error) {
@@ -117,7 +120,7 @@ class _AddEventState extends State<AddEvent> {
           confirmBtnColor: Colors.green,
           onConfirmBtnTap: () {
             var docRef = FirebaseFirestore.instance
-                .collection('EventType')
+                .collection('event_types')
                 .doc(documentId)
                 .delete();
             Navigator.of(context).pop();
@@ -167,14 +170,9 @@ class _AddEventState extends State<AddEvent> {
                         padding: const EdgeInsets.all(8.0),
                         child: Row(
                           children: [
-                            Text('المناسبات',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontFamily: 'Amiri',
-                                    fontSize: 28,
-                                    fontStyle: FontStyle.italic,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color.fromARGB(255, 60, 19, 60))),
+                            Text(
+                              'المناسبات',
+                            ),
                             SizedBox(
                               width: 220,
                             ),
@@ -193,8 +191,9 @@ class _AddEventState extends State<AddEvent> {
                       obscureTextField: false,
                     ),
                     DropdownButton<String>(
-                      value:
-                          classificationList.isNotEmpty ? dropdownValue : null,
+                      value: classificationList.isNotEmpty
+                          ? dropdownValue
+                          : 'no item',
                       icon: const Icon(Icons.arrow_downward),
                       elevation: 16,
                       style: const TextStyle(color: Colors.deepPurple),
@@ -217,15 +216,6 @@ class _AddEventState extends State<AddEvent> {
                       }).toList(),
                     ),
                     TextFieldDesign(
-                      Text: 'الخدمات المتعلقة بالمناسبة:',
-                      icon: Icons.room_service,
-                      ControllerTextField: ControllerSer,
-                      onChanged: (value) {
-                        serviceType = value.split(',');
-                      },
-                      obscureTextField: false,
-                    ),
-                    TextFieldDesign(
                       Text: 'إضافة صورة:',
                       icon: Icons.image,
                       ControllerTextField: ControllerImage,
@@ -239,33 +229,14 @@ class _AddEventState extends State<AddEvent> {
                       margin: EdgeInsets.only(bottom: 90),
                       child: FloatingActionButton(
                         backgroundColor: ColorPink_100,
-                        child: Text('إضافة مناسبة',
-                            style: TextStyle(
-                                fontFamily: 'Amiri',
-                                fontSize: 18,
-                                fontStyle: FontStyle.italic,
-                                color: Colors.white)),
+                        child: Text('إضافة مناسبة'),
                         onPressed: () async {
                           QuerySnapshot<Map<String, dynamic>> querySnapshot =
                               await FirebaseFirestore.instance
-                                  .collection('Classifications')
-                                  .where('Name', isEqualTo: classification)
+                                  .collection('event_classificaion_types')
+                                  .where('description',
+                                      isEqualTo: classification)
                                   .get();
-
-                          EventType newEvent = EventType(
-                              Name: name,
-                              ServiceType: serviceType,
-                              Classification: classification,
-                              imageUrl: ImageUrl);
-                          newEvent.addToFirestore();
-
-                          ControllerName.clear();
-                          ControllerSer.clear();
-                          ControllerImage.clear();
-                          QuickAlert.show(
-                            context: context,
-                            type: QuickAlertType.success,
-                          );
                         },
                       ),
                     ),
@@ -291,12 +262,13 @@ class _AddEventState extends State<AddEvent> {
     );
   }
 
+//display the event type exist
   SafeArea EventScreen() {
     return SafeArea(
       child: Material(
         child: StreamBuilder<QuerySnapshot>(
           stream:
-              FirebaseFirestore.instance.collection('EventType').snapshots(),
+              FirebaseFirestore.instance.collection('event_types').snapshots(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Center(child: CircularProgressIndicator());
@@ -316,8 +288,8 @@ class _AddEventState extends State<AddEvent> {
                 itemBuilder: (context, index) {
                   final doc = eventDocs[index];
                   return EventItemDisplay(
-                    title: doc['Name'].toString(),
-                    imageUrl: doc['imageUrl'].toString(),
+                    title: doc['name'].toString(),
+                    imageUrl: doc['image_url'].toString(),
                     id: doc.id,
                     onTapFunction: () {
                       getDataById(doc.id);
