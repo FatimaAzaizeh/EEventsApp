c316a7c9a8209ea9aec304e8f3e6b49aaa32791f
Raneemkalbouneh <162698624+Raneemkalbouneh@users.noreply.github.com>
2024-05-11 13:00:43 +0300

    Update Add_Service.dart

--- lib/screens/Admin/widgets_admin/Add_Service.dart
+++ lib/screens/Admin/widgets_admin/Add_Service.dart
@@ -1,15 +1,12 @@
 import 'dart:typed_data';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// Your imports
 import 'package:testtapp/constants.dart';
 import 'package:testtapp/models/ServiceType.dart';
-import 'package:quickalert/models/quickalert_type.dart';
-import 'package:quickalert/widgets/quickalert_dialog.dart';
-import 'package:testtapp/screens/Admin/widgets_admin/TexFieldDesign.dart';
 import 'package:testtapp/widgets/Event_item.dart';
 
 final _firestore = FirebaseFirestore.instance;
@@ -44,202 +41,210 @@ class _AddServiceState extends State<AddService> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Color.fromARGB(221, 255, 255, 255)),
-            width: 500,
-            height: double.maxFinite,
-            child: Column(
-              children: [
-                Container(
-                  width: double.maxFinite,
-                  height: 70,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
+    return Scaffold(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color.fromARGB(221, 255, 255, 255),
+                ),
+                width: 500,
+                height: double.maxFinite,
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.maxFinite,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30))),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Text(
-                          'الخدمات',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontFamily: 'Amiri',
-                              fontSize: 28,
-                              fontStyle: FontStyle.italic,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 60, 19, 60)),
+                          topRight: Radius.circular(30),
                         ),
-                        SizedBox(
-                          width: 220,
-                        ),
-                        TextButton(
-                          onPressed: showEditButton
-                              ? () async {
-                                  if (fileName != 'لم يتم اختيار صورة ') {
-                                    await uploadFile();
-                                  }
-                                  ServiceType.updateServiceFirestore(
-                                      ControllerName.text,
-                                      imageUrl,
-                                      ControllerId.text);
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              'الخدمات',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'Amiri',
+                                fontSize: 28,
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 60, 19, 60),
+                              ),
+                            ),
+                            SizedBox(width: 220),
+                            TextButton(
+                              onPressed: showEditButton
+                                  ? () async {
+                                      if (fileName != 'لم يتم اختيار صورة ') {
+                                        await uploadFile();
+                                      }
+                                      ServiceType.updateServiceFirestore(
+                                          ControllerName.text,
+                                          imageUrl,
+                                          ControllerId.text);
 
-                                  setState(() {
-                                    showEditButton = false;
-                                  });
+                                      setState(() {
+                                        showEditButton = false;
+                                      });
+                                      ControllerName.clear();
+                                      ControllerId.clear();
+
+                                      QuickAlert.show(
+                                        context: context,
+                                        type: QuickAlertType.success,
+                                      );
+                                      setState(() {
+                                        showEditButton = false;
+                                      });
+                                    }
+                                  : null,
+                              child: Icon(Icons.edit),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
                                   ControllerName.clear();
                                   ControllerId.clear();
-
-                                  QuickAlert.show(
-                                    context: context,
-                                    type: QuickAlertType.success,
-                                  );
-                                  setState(() {
-                                    showEditButton = false;
-                                  });
-                                }
-                              : null,
-                          child: Icon(Icons.edit),
+                                  ControllerImage.clear();
+                                });
+                              },
+                              child: Icon(Icons.clear),
+                            ),
+                          ],
                         ),
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'أسم الخدمة:',
+                        icon: Icon(Icons.title),
+                      ),
+                      controller: ControllerName,
+                      onChanged: (value) {
+                        name = value; // Update the Name variable
+                        // Update the serviceType list
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'رقم الخدمة',
+                        icon: Icon(Icons.room_service),
+                      ),
+                      controller: ControllerId,
+                      onChanged: (value) {
+                        value;
+                      },
+                      enabled: !showEditButton,
+                    ),
+                    Row(
+                      children: [
                         TextButton(
-                          onPressed: () {
-                            setState(() {
-                              ControllerName.clear();
-                              ControllerId.clear();
-                              ControllerImage.clear();
-                            });
+                          onPressed: () async {
+                            FilePickerResult? result =
+                                await FilePicker.platform.pickFiles();
+                            if (result != null) {
+                              setState(() async {
+                                fileBytes = result.files.first.bytes;
+                                fileName = result.files.first.name;
+                                await uploadFile();
+                              });
+                            }
                           },
-                          child: Icon(Icons.clear),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Tooltip(
+                                message: 'إضافة صورة',
+                                child: Icon(
+                                  Icons.add,
+                                  size: 34,
+                                  color: ColorPurple_100,
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                fileName,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: fileBytes != null
+                                      ? ColorPurple_100
+                                      : Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ),
-                TextFieldDesign(
-                  Text: 'أسم الخدمة:',
-                  icon: Icons.title,
-                  ControllerTextField: ControllerName,
-                  onChanged: (value) {
-                    name = value; // Update the Name variable
-                    // Update the serviceType list
-                  },
-                  obscureTextField: false,
-                  enabled: true,
-                ),
-                TextFieldDesign(
-                  Text: 'رقم الخدمة',
-                  icon: Icons.room_service,
-                  ControllerTextField: ControllerId,
-                  onChanged: (value) {
-                    value;
-                  },
-                  obscureTextField: false,
-                  enabled: !showEditButton,
-                ),
-                Row(
-                  children: [
-                    TextButton(
-                      onPressed: () async {
-                        FilePickerResult? result =
-                            await FilePicker.platform.pickFiles();
-                        if (result != null) {
-                          setState(() async {
-                            fileBytes = result.files.first.bytes;
-                            fileName = result.files.first.name;
-                            await uploadFile();
+                    Container(
+                      width: double.maxFinite,
+                      margin: EdgeInsets.only(bottom: 90),
+                      child: FloatingActionButton(
+                        backgroundColor: ColorPink_100,
+                        child: Text(
+                          'إضافة خدمة',
+                          style: TextStyle(
+                            fontFamily: 'Amiri',
+                            fontSize: 18,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () async {
+                          setState(() {
+                            showSpinner = true;
                           });
-                        }
-                      },
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Tooltip(
-                            message: 'إضافة صورة',
-                            child: Icon(
-                              Icons.add,
-                              size: 34,
-                              color: ColorPurple_100,
-                            ),
-                          ),
-                          SizedBox(width: 8),
-                          Text(
-                            fileName,
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: fileBytes != null
-                                  ? ColorPurple_100
-                                  : Colors.grey,
-                            ),
-                          ),
-                        ],
+
+                          ServiceType newservice = ServiceType(
+                            id: ControllerId.text,
+                            name: ControllerName.text,
+                            imageUrl: imageUrl,
+                          );
+                          newservice.saveToDatabase();
+                          setState(() {
+                            showSpinner = false;
+                          });
+                          ControllerName.clear();
+                          ControllerId.clear();
+                          ControllerImage.clear();
+                          QuickAlert.show(
+                            context: context,
+                            type: QuickAlertType.success,
+                          );
+                        },
                       ),
                     ),
                   ],
                 ),
-                Container(
-                  width: double.maxFinite,
-                  margin: EdgeInsets.only(bottom: 90),
-                  child: FloatingActionButton(
-                    backgroundColor: ColorPink_100,
-                    child: Text(
-                      'إضافة خدمة',
-                      style: TextStyle(
-                        fontFamily: 'Amiri',
-                        fontSize: 18,
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                    onPressed: () async {
-                      setState(() {
-                        showSpinner = true;
-                      });
-
-                      ServiceType newservice = ServiceType(
-                        id: ControllerId.text,
-                        name: ControllerName.text,
-                        imageUrl: imageUrl,
-                      );
-                      newservice.saveToDatabase();
-                      setState(() {
-                        showSpinner = false;
-                      });
-                      ControllerName.clear();
-                      ControllerId.clear();
-                      ControllerImage.clear();
-                      QuickAlert.show(
-                        context: context,
-                        type: QuickAlertType.success,
-                      );
-                    },
-                  ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
                 ),
-              ],
+                width: 400,
+                height: double.maxFinite,
+                child: ServiceScreen(),
+              ),
             ),
-          ),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
-            ),
-            width: 400,
-            height: double.maxFinite,
-            child: ServiceScreen(),
-          ),
-        ),
-      ],
+      ),
     );
   }
 
@@ -322,4 +327,42 @@ class _AddServiceState extends State<AddService> {
       ),
     );
   }
+}
+
+// QuickAlert class
+class QuickAlert {
+  static void show(
+      {required BuildContext context, required QuickAlertType type}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title:
+              type == QuickAlertType.success ? Text('Success') : Text('Error'),
+          content: type == QuickAlertType.success
+              ? Text('Operation successful!')
+              : Text('Error occurred.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+// QuickAlertType enum
+enum QuickAlertType { success, error }
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: AddService(),
+    ),
+  ));
 }
